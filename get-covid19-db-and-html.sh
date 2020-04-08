25c65327519ca83251ad1ff76f08b5fe213617d4
Frederic Bergeron <bergeron@nlp.ist.i.kyoto-u.ac.jp>
2020-04-08 14:19:59 +0900

    Change html dir perms after extracting HTML files.

--- get-covid19-db-and-html.sh
+++ get-covid19-db-and-html.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-#
-# Important: 
-#
-# This script must be executed from from tulip.
-# It uses my local wget-1.20.
-# The version installed on the system had this issue:
-# idn_encode failed (3): ‘Non-digit/letter/hyphen in input’
-# https://stackoverflow.com/questions/37881346/idn-encode-failed-3-non-digit-letter-hyphen-in-input
-# Using a more recent version seems to bypass this issue.
-#
-
 export http_proxy=http://proxy.kuins.net:8080
 export https_proxy=http://proxy.kuins.net:8080
 export HTTP_PROXY=http://proxy.kuins.net:8080
@@ -20,6 +9,7 @@ export FTP_PROXY=http://proxy.kuins.net:8080
 
 SCRIPTS=/home/frederic/covid19/translation
 PIPENV=/home/frederic/.local/bin/pipenv
+HTML_DIR=/mnt/hinoki/share/covid19/html
 
 cd "$SCRIPTS"
 
@@ -30,5 +20,7 @@ echo "The db files have been fetched."
 echo "Extracting HTML files from db files..."
 $PIPENV run python extract-html.py
 echo "The HTML files have been extracted."
+
+find $HTML_DIR -type d -exec chmod 775 {} \;
 
 echo "Done"
