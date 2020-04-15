7ca001ca8a2e26d12261714ccf7eb85bad552d8f
Frederic Bergeron <bergeron@nlp.ist.i.kyoto-u.ac.jp>
2020-04-15 13:45:03 +0900

    Fixed environment so that it can work from cron.

--- convert-html-to-xml.sh
+++ convert-html-to-xml.sh
@@ -3,10 +3,13 @@
 SCRIPTS=/home/frederic/covid19/translation
 PIPENV=/home/frederic/.local/bin/pipenv
 
+export PATH=/orange/ubrew/data/bin:$PATH
+export PERL5LIB=/home/frederic/usr/lib/perl:/home/frederic/usr/lib/perl/lib/perl5:/home/frederic/usr/lib/perl/lib/perl5/x86_64-linux-thread-multi:/home/frederic/perl5:/home/frederic/perl5/lib/perl5:/home/frederic/perl5/lib/perl5/site_perl/5.26.2:. 
+
 cd "$SCRIPTS"
 
 echo "Converting HTML files to XML..."
-$PIPENV run python convert-html-to-xml.py
+$PIPENV run python convert-html-to-xml-2.py
 echo "The HTML files have converted."
 
 echo "Done"
