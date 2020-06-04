bc2273c66a3382f6b04117160cec718239429795
Frederic Bergeron <bergeron@nlp.ist.i.kyoto-u.ac.jp>
2020-06-04 18:16:04 +0900

    Run forever.

--- convert-html-to-xml-2.py
+++ convert-html-to-xml-2.py
@@ -1,32 +1,27 @@
 import datetime
 import json
+import logging
+import logging.config
 import os
 import queue
+import signal
 import subprocess
 import sys
 import threading
 import time
 
-config_filename = sys.argv[1] if len(sys.argv) == 2 else 'config.json'
-with open(config_filename, 'r') as config_file:
-    config = json.load(config_file)
 
-db_dir = config['db_dir']
-html_dir = config['html_dir']
-xml_dir = config['xml_dir']
-run_dir = config['run_dir']
-www2sf_dir = config['WWW2sf_dir']
-detectblocks_dir = config['detectblocks_dir']
-converter_count = 10
+def signal_handler(sig, frame):
+    print("Ctrl+C has been pressed. Let's stop the workers.")
+    global stopped
+    stopped = True
+    for producer in producers:
+        producer.stopped = True
+    for convert in converters:
+        convert.stopped = True
+    print("The converter script should stop in a few moments.  Please be patient.")
 
 
-now = datetime.datetime.now()
-
-queue_html_files = queue.Queue()
-
-processed_files = []
-
-mutex = threading.Lock()
 
 class Producer(threading.Thread):
 
@@ -34,9 +29,10 @@ class Producer(threading.Thread):
         threading.Thread.__init__(self)
         self.name = "Producer for region {}".format(region)
         self.region = region
+        self.stopped = False
 
     def run(self):
-        print("Processing files from region: {}...".format(self.region))
+        logger.info("Processing files from region: {}...".format(self.region))
         root_input_dir = "{0}/ja_translated".format(self.region)
         root_output_dir = "{0}/ja_translated".format(self.region)
 
@@ -44,16 +40,21 @@ class Producer(threading.Thread):
         root_abs_output_dir = os.path.join(xml_dir, root_output_dir)
         if os.path.exists(root_abs_input_dir):
             for domain in os.listdir(root_abs_input_dir):
-                print("Processing domain: {}...".format(domain))
+                logger.info("Processing domain: {}...".format(domain))
                 for top, dirs, files in os.walk(os.path.join(root_abs_input_dir, domain)):
-                    # print("top={0} dirs={1} files={2}".format(top, dirs, files))
+                    # logger.debug("top={0} dirs={1} files={2}".format(top, dirs, files))
                     for file in files:
+
+                        if self.stopped:
+                            logger.info("Producer {0} has been stopped.".format(self.identifier))
+                            return
+
                         if file.endswith('.html'):
                             if not queue_html_files.full():
                                 www2sf_input_file = os.path.join(top, file)
                                 www2sf_output_file = os.path.join(root_abs_output_dir, domain, top[top.index(domain) + len(domain) + 1:], file[:file.index('.html')] + '.xml')
 
-                                print("Adding {} to queue.".format(www2sf_input_file))
+                                # logger.info("Adding {} to queue.".format(www2sf_input_file))
                                 queue_html_files.put((www2sf_input_file, www2sf_output_file))
 
 
@@ -63,23 +64,28 @@ class Converter(threading.Thread):
         threading.Thread.__init__(self)
         self.name = "Converter: {}".format(identifier)
         self.identifier = identifier
+        self.stopped = False
 
     def run(self):
         while True:
             try:
+                if self.stopped:
+                    logger.info("Converter {0} has been stopped.".format(self.identifier))
+                    break
+
                 if not queue_html_files.empty():
                     (www2sf_input_file, www2sf_output_file) = queue_html_files.get()
-                    print("Process file: {}".format(www2sf_input_file))
+                    # logger.info("Process file: {}".format(www2sf_input_file))
                     if not os.path.exists(www2sf_output_file):
-                        print("Convert input={} output={}".format(www2sf_input_file, www2sf_output_file))
-                        print("cd {0} && tool/html2sf.sh -T -D {1} -J {2}".format(www2sf_dir, detectblocks_dir, www2sf_input_file))
+                        logger.info("Convert input={} output={}".format(www2sf_input_file, www2sf_output_file))
+                        logger.info("cd {0} && tool/html2sf.sh -T -D {1} -J {2}".format(www2sf_dir, detectblocks_dir, www2sf_input_file))
                         process = subprocess.run(["tool/html2sf.sh", "-T", "-D {}".format(detectblocks_dir), "-J", www2sf_input_file], cwd=www2sf_dir, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                        print("return_code={0}".format(process.returncode))
+                        logger.info("return_code={0}".format(process.returncode))
                         if process.returncode == 0:
                             os.makedirs(www2sf_output_file[:www2sf_output_file.rindex('/')], exist_ok=True)
                             with open(www2sf_output_file, "wb") as xml_file:
                                 xml_file.write(process.stdout)
-                            print("Output file {}: OK".format(www2sf_output_file))
+                            logger.info("Output file {}: OK".format(www2sf_output_file))
                             mutex.acquire()
                             try:
                                 processed_files.append(www2sf_output_file)
@@ -92,31 +98,69 @@ class Converter(threading.Thread):
                         break
             except:
                 e = sys.exc_info()[0]
-                print("An error has occurred: %s" % e)
+                logger.info("An error has occurred: %s" % e)
 
 
-converters = []
-for c in range(0, converter_count):
-    converter = Converter(c)
-    converters.append(converter)
-    converter.start()
+if __name__ == '__main__':
+    import argparse
 
-producers = []
-for region in os.listdir(html_dir):
-    producer = Producer(region)
-    producers.append(producer)
-    producer.start()
+    parser = argparse.ArgumentParser(description="Launch the converter.", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--config", help="Location of config file.", type=str, default="config.json")
+    parser.add_argument("--log_config", help="Log configuration file.", type=str, default="converter_logging.conf")
+    args = parser.parse_args(args=None)
 
-for producer in producers:
-    producer.join()
+    config_filename = args.config
 
-for convert in converters:
-    converter.join()
+    logging.config.fileConfig(args.log_config)
+    logger = logging.getLogger('default')
 
-new_xml_filename = os.path.join(run_dir, 'new-xml-files-{0}.txt'.format(now.strftime('%Y-%m-%d-%H-%M')))
-print("Writing report file: {0} new_xml_file_count: {1}".format(new_xml_filename, len(processed_files)))
-with open(new_xml_filename, 'a') as new_xml_file:
-    for file in processed_files:
-        new_xml_file.write(file)
-        new_xml_file.write("\n")
-print("Report written.")
+    signal.signal(signal.SIGINT, signal_handler)
+    print("Hit CTRL+C and wait a little bit to stop the converter.")
+
+    stopped = False
+    while not stopped:
+
+        with open(config_filename, 'r') as config_file:
+            config = json.load(config_file)
+
+        html_dir = config['html_dir']
+        xml_dir = config['xml_dir']
+        run_dir = config['run_dir']
+        www2sf_dir = config['WWW2sf_dir']
+        detectblocks_dir = config['detectblocks_dir']
+        converter_count = 12
+
+        now = datetime.datetime.now()
+
+        queue_html_files = queue.Queue()
+
+        processed_files = []
+
+        mutex = threading.Lock()
+
+        converters = []
+        for c in range(0, converter_count):
+            converter = Converter(c)
+            converters.append(converter)
+            converter.start()
+
+        producers = []
+        for region in os.listdir(html_dir):
+            producer = Producer(region)
+            producers.append(producer)
+            producer.start()
+
+        for producer in producers:
+            producer.join()
+
+        for convert in converters:
+            converter.join()
+
+        if len(processed_files) > 0:
+            new_xml_filename = os.path.join(run_dir, 'new-xml-files-{0}.txt'.format(now.strftime('%Y-%m-%d-%H-%M')))
+            logger.info("Writing report file: {0} new_xml_file_count: {1}".format(new_xml_filename, len(processed_files)))
+            with open(new_xml_filename, 'a') as new_xml_file:
+                for file in processed_files:
+                    new_xml_file.write(file)
+                    new_xml_file.write("\n")
+            logger.info("Report written.")
