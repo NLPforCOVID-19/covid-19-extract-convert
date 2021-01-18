b403c8e2aa7de7e9c95a0a6d26acb08d3ae6527c
Frederic Bergeron <bergeron@nlp.ist.i.kyoto-u.ac.jp>
2021-01-18 17:51:05 +0900

    Generated html and stats.

--- extract-tweets.py
+++ extract-tweets.py
@@ -23,71 +23,66 @@ processed_databases = []
 
 def write_tweet_data(tweet_id):
     print(f"write_tweet_data tweet_id={tweet_id}")
-    tweet = tweets[tweet_id]
-    tweet_timestamp = datetime.datetime.fromtimestamp(tweet['status'].created_at_in_seconds)
-    country_code_dir = utils.get_country_code_dir(tweet['country_code'])
-    timestamp_path = tweet_timestamp.strftime('%Y/%m/%d/%H-%M')
-    html_orig_tweet_dir = os.path.join(twitter_html_dir, country_code_dir, "orig", timestamp_path)
-    os.makedirs(html_orig_tweet_dir, exist_ok=True)
-    temp_path = html_orig_tweet_dir
-    while temp_path != twitter_html_dir:
-        os.chmod(temp_path, 0o775)
-        temp_path = os.path.dirname(temp_path)
+    try:
+        tweet = tweets[tweet_id]
+        timestamp = now.strftime('%Y-%m-%d-%H-%M')
+        tweet_timestamp = datetime.datetime.fromtimestamp(tweet['status'].created_at_in_seconds)
+        country_code_dir = utils.get_country_code_dir(tweet['country_code'])
+        timestamp_path = tweet_timestamp.strftime('%Y/%m/%d/%H-%M')
+        html_orig_tweet_dir = os.path.join(twitter_html_dir, country_code_dir, "orig", timestamp_path)
+        os.makedirs(html_orig_tweet_dir, exist_ok=True)
+        temp_path = html_orig_tweet_dir
+        while temp_path != twitter_html_dir:
+            os.chmod(temp_path, 0o775)
+            temp_path = os.path.dirname(temp_path)
 
-    json_filename = os.path.join(html_orig_tweet_dir, f"{tweet_id}.json")
-    with open(json_filename, 'w') as json_file:
-        json.dump(tweet['json'], json_file)
+        json_filename = os.path.join(html_orig_tweet_dir, f"{tweet_id}.json")
+        with open(json_filename, 'w') as json_file:
+            json.dump(tweet['json'], json_file)
 
-    metadata_filename = os.path.join(html_orig_tweet_dir, f"{tweet_id}.metadata")
-    with open(metadata_filename, 'w') as metadata_file:
-        metadata = {
-            "id": tweet_id,
-            "country": tweet["country"],
-            "country_code": tweet["country_code"],
-            "count": tweet["count"]
-        }
-        json.dump(metadata, metadata_file)
+        metadata_filename = os.path.join(html_orig_tweet_dir, f"{tweet_id}.metadata")
+        with open(metadata_filename, 'w') as metadata_file:
+            metadata = {
+                "id": tweet_id,
+                "country": tweet["country"],
+                "country_code": tweet["country_code"],
+                "count": tweet["count"]
+            }
+            json.dump(metadata, metadata_file)
 
-    if country_code_dir in tweet_count_per_country:
-        tweet_count_per_country[country_code_dir] += 1
-    else:
-        tweet_count_per_country[country_code_dir] = 1
+        tweet_text_for_html = tweet['status'].full_text if tweet['status'].tweet_mode == 'extended' else tweet['status'].text
+        tweet_text_for_html = re.sub("https://t.co/\w+", "", tweet_text_for_html) # Remove urls from text.
+        tweet_text_for_html = re.sub("#\w+\s*", "", tweet_text_for_html) # Remove hashtags from text.
+        html_filename = os.path.join(html_orig_tweet_dir, f"{tweet_id}.html")
+        with open(html_filename, 'w') as html_file:
+            html_str = (
+                "<!DOCTYPE html>\n"
+                "<html lang=\"ja\">\n"
+                "<head><meta charset=\"utf-8\"/></head>\n"
+                "<body>\n"
+                f"<p>{tweet_text_for_html}</p>\n"
+                "</body>\n"
+                "</html>\n"
+            )
+            html_file.write(html_str)
+
+        new_html_filename = os.path.join(run_dir, 'new-html-files', f'new-twitter-html-files-{timestamp}.txt')
+        with open(new_html_filename, 'a') as new_html_file:
+            new_html_file.write(html_filename)
+            new_html_file.write("\n")
+
+        if country_code_dir in tweet_count_per_country:
+            tweet_count_per_country[country_code_dir] += 1
+        else:
+            tweet_count_per_country[country_code_dir] = 1
+    except OSError as os_err:
+        print(f"An error has occurred in write_tweet_data(tweet_id={tweet_id}) os_err={os_err}")
 
 
-# def write_html_file(path, filename, url, content, source, domain_path):
-#     print("write_html_file path={0} filename={1} url={2} source={3} domain_path={4}".format(path, filename, url, source, domain_path))
-#     try:
-#         timestamp = now.strftime('%Y-%m-%d-%H-%M')
-#         timestamp_path = now.strftime('%Y/%m/%d-%H-%M')
-#         path = "{0}/{1}".format(path, timestamp_path).replace("//", "/")
-#         os.makedirs(path, exist_ok=True)
-#         temp_path = path
-#         while temp_path != domain_path:
-#             os.chmod(temp_path, 0o775)
-#             temp_path = os.path.dirname(temp_path)
-#         filename = os.path.join(path, filename)
-#         with open(filename, 'wb') as html_file:
-#             html_file.write(content)
-#         filename_url = filename[:-5] + '.url'
-#         with open(filename_url, 'w') as url_file:
-#             url_file.write(url)
-#         filename_source = filename[:-5] + '.src'
-#         with open(filename_source, 'w') as source_file:
-#             source_file.write(source)
-#         new_html_filename = os.path.join(run_dir, 'new-html-files', 'new-html-files-{}.txt'.format(timestamp))
-#         with open(new_html_filename, 'a') as new_html_file:
-#             new_html_file.write(filename)
-#             new_html_file.write("\n")
-#         global nb_html_files
-#         nb_html_files += 1
-#     except OSError as os_err:
-#         print("An error has occurred in write_html(path={0} filename={1} url={2}): {3}".format(path, filename, url, os_err))
-# 
-# 
-# def write_stats_file(filename):
-#     if nb_html_files_per_domain:
-#         with open(filename, 'w') as stats_file:
-#             json.dump(nb_html_files_per_domain, stats_file)
+def write_stats_file(filename, tweet_count_per_country):
+    if len(tweet_count_per_country) > 0:
+        with open(filename, 'w') as stats_file:
+            json.dump(tweet_count_per_country, stats_file)
 
 
 def process_tweet(tweet_id, tweet_count, tweet_lang, tweet_country, tweet_json_str):
@@ -150,13 +145,13 @@ for db_filename in sorted(glob.glob(f'{twitter_db_dir}/tweets_*.txt')):
 
     print(f"tweet_count_per_country={tweet_count_per_country}")
 
-    # if os.path.exists(db_filename):
-    #     os.remove(db_filename)
+    stats_file = os.path.join(run_dir, 'stats', f"twitter-stats-{now.strftime('%Y-%m-%d-%H-%M')}.json")
+    write_stats_file(stats_file, tweet_count_per_country)
 
-# with open(run_filename, 'w') as run_file:
-#     json.dump(processed_databases, run_file)
+    # Remove database to save disk space.
+    if os.path.exists(db_filename):
+        os.remove(db_filename)
 
-
-# Write some stats.
-# stats_file = os.path.join(run_dir, 'stats', 'stats-{}.json'.format(now.strftime('%Y-%m-%d-%H-%M')))
-# write_stats_file(stats_file)
+# Remember that the file has been processed.
+with open(run_filename, 'w') as run_file:
+    json.dump(processed_databases, run_file)
