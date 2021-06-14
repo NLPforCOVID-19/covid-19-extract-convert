ce92c05a5b3fa2cfb10b21e34329585ea5d1f95a
Frederic Bergeron <bergeron@nlp.ist.i.kyoto-u.ac.jp>
2021-06-14 14:44:35 +0900

    Remove pause between checks to extract more tweets

--- twitter-extracter.py
+++ twitter-extracter.py
@@ -14,7 +14,8 @@ import traceback
 
 
 # Number of seconds to wait before checking again for new twitter data.
-INTER_CHECKS_DELAY = 60 * 60 * 2
+# INTER_CHECKS_DELAY = 60 * 60 * 2
+INTER_CHECKS_DELAY = 0
 
 exit = Event()
 
